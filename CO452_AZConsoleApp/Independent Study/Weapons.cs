cae2b47444105b9c5d20bf818027e5facda9a171
Ana Zorro <al.petingazorro@gmail.com>
2018-12-30 18:48:31 +0000

    Weapons Code

--- CO452_AZConsoleApp/Independent Study/Weapons.cs
+++ CO452_AZConsoleApp/Independent Study/Weapons.cs
@@ -8,16 +8,34 @@ namespace CO452_AZConsoleApp.Independent_Study
 {
     class Weapons
     {
-        private const int redTokens = 5;
-        private const int greenTokens = 10;
-        private const int blueTokens = 20;
-        private string name;
-
+        private static int redTokens;
+        private static int greenTokens;
+        private static int blueTokens;
+        private static string name;
+        private static int totalCredit;
+        private static int arrow = 50;
+        private static int howManyArrows;
+        private static int creditsLeft;
+                      
         public static void Store()
         {
             name = UserLib.GetString("Enter your name > ");
 
+            redTokens = UserLib.GetInt("How many red tokens do you have? ");
+            greenTokens = UserLib.GetInt("How many green tokens do you have? ");
+            blueTokens = UserLib.GetInt("How many blue tokens do you have? ");
+            Console.WriteLine("");
 
+            totalCredit = (redTokens * 5) + (greenTokens * 10) + (blueTokens * 20);
+            Console.WriteLine("The total credit available is " + totalCredit);
+            Console.WriteLine("");
+
+            howManyArrows = totalCredit / arrow;
+            Console.WriteLine("In total, you can buy " + howManyArrows + " arrows. ");
+            Console.WriteLine("");
+
+            creditsLeft = totalCredit % arrow;
+            Console.WriteLine(name + ", you have purchased " + howManyArrows + " and have " + creditsLeft + " credits left. ");
         }
     }
 }
